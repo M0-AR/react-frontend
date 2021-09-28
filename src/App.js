906958ffd901e3a48e80336223ce9a2f7a8803b9
m0-ar <s176492@student.dtu.dk>
2021-09-28 11:01:37 +0200

    Sidebar and Container are done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Login from "./Login";
+import Sidebar  from './component/dashboard/Sidebar';
+import Container from './component/dashboard/Container';
 
 function App() {
   return (
-    <div className="App">
-        <Login/>
+    <div>
+      <div className="flex w-screen h-screen" >
+        <Sidebar />
+        <div className="w-screen ">
+          <Container />
+          {/* <Header /> */}
+        </div>
+      </div>
+
     </div>
   );
 }
