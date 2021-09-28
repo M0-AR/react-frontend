99d0a34f14e305d8b9eec64fbf2e23ceb758c942
m0-ar <s176492@student.dtu.dk>
2021-09-28 17:25:14 +0200

    Change the navigation: Sign in -> Sign Up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Login from "./Login";
-
+// import Login from "./Login";
+import Registration from './Registration';
 function App() {
   return (
     <div className="App">
-        <Login/>
+        {/* <Login/> */}
+        <Registration />
     </div>
   );
 }
