6ea39d963e712c155e33237ddfdd46b1220cdf3e
m0-ar <s176492@student.dtu.dk>
2021-10-23 18:51:13 +0200

    Display service's objects in table

--- src/components/dashboard/Service/Services.js
+++ src/components/dashboard/Service/Services.js
@@ -1,20 +1,39 @@
-import React from 'react'
-import ServiceForm from './ServiceForm'
-import { Paper, makeStyles } from '@material-ui/core';
+import React, { useState } from 'react'
+import { Paper, makeStyles, TableBody, TableRow, TableCell} from '@material-ui/core';
+import * as bookService from './services/bookService'
+import useTable from './useTable';
 
 const useStyles = makeStyles(theme => ({
     pageContent: {
-        margin: theme.spacing(5)
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
     }
 }))
 
 export default function Services() {
 
     const classes = useStyles();
+    const [records, setRecords] = useState(bookService.getAllServices())
+    const {TblContainer} = useTable()
 
     return (
         <Paper className={classes.pageContent}>
-            <ServiceForm />
+            {/* <ServiceForm /> */}
+            <TblContainer>
+                <TableBody>
+                    {
+                        records.map(item => (
+                            <TableRow key={item.id}> 
+                                <TableCell>{item.title}</TableCell>
+                                <TableCell>{item.price}</TableCell>
+                                <TableCell>{item.isAvailable}</TableCell>
+                                <TableCell>{item.serviceStartDate}</TableCell>
+                            </TableRow>
+
+                        ))
+                    }
+                </TableBody>
+            </TblContainer>
         </Paper>
     )
 }
