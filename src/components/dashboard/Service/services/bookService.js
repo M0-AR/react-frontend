9409926489eeb47c7ca7fdbcc98f02bd09436706
m0-ar <s176492@student.dtu.dk>
2021-11-14 11:03:21 +0100

    Temp: use localstoarge

--- src/components/dashboard/Service/services/bookService.js
+++ src/components/dashboard/Service/services/bookService.js
@@ -8,18 +8,24 @@ const KEYS ={
 const baseUrl = process.env.NODE_ENV === '' ? // development
     "http://localhost:10000/":"https://api.comit.dev/"; // Check if dev environment
 
+// export function insertService(data) {
+//     //data['id'] = generateServiceId()
+//     let services=getAllServices();
+//     services.push(data)
+//     axios.post(baseUrl + "api/v1/go-mongo/dashboard/add", data)
+//           .then(res=> {
+//               console.log(res)
+//           })
+//           .catch(err=>{
+//               console.log(err)
+//           })
+//     localStorage.setItem(KEYS.services,JSON.stringify(services))
+// }
 export function insertService(data) {
-    //data['id'] = generateServiceId()
-    let services=getAllServices();
-    services.push(data)
-    axios.post(baseUrl + "api/v1/go-mongo/dashboard/add", data)
-          .then(res=> {
-              console.log(res)
-          })
-          .catch(err=>{
-              console.log(err)
-          })
-    localStorage.setItem(KEYS.services,JSON.stringify(services))
+    data['id'] = generateServiceId()
+    let employees=getAllServices();
+    employees.push(data)
+    localStorage.setItem(KEYS.services,JSON.stringify(employees))
 }
 
 export function generateServiceId() {
@@ -43,18 +49,23 @@ export function deleteService(id) {
     localStorage.setItem(KEYS.services, JSON.stringify(services));
 }
 
+// export function getAllServices() {
+//     //cleanLocalStoarge()
+//     if (localStorage.getItem(KEYS.services) == null) {
+//         axios.get(baseUrl + "api/v1/go-mongo/dashboard/list")
+//           .then(res=> {
+//               console.log(res.data.services)
+//               localStorage.setItem(KEYS.services, JSON.stringify(res.data.services))
+//           })
+//           .catch(err=>{
+//               console.log(err)
+//           })
+//     } 
+//     return JSON.parse(localStorage.getItem(KEYS.services));
+// }
 export function getAllServices() {
-    //cleanLocalStoarge()
-    if (localStorage.getItem(KEYS.services) == null) {
-        axios.get(baseUrl + "api/v1/go-mongo/dashboard/list")
-          .then(res=> {
-              console.log(res.data.services)
-              localStorage.setItem(KEYS.services, JSON.stringify(res.data.services))
-          })
-          .catch(err=>{
-              console.log(err)
-          })
-    } 
+    if (localStorage.getItem(KEYS.services) == null)
+        localStorage.setItem(KEYS.services, JSON.stringify([]))
     return JSON.parse(localStorage.getItem(KEYS.services));
 }
 
