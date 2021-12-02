65fba60da3e143a4c97108cd4e2ea2514e480a1a
B1gDaddyKane <jenschrj97@gmail.com>
2021-12-02 20:10:16 +0100

    Removed tailwind images

--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -5,11 +5,6 @@ const Registration = () => {
         <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
             <div className="max-w-md w-full space-y-8">
                 <div>
-                    <img
-                    className="mx-auto h-12 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                    alt="Workflow"
-                    />
                     <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Sign up</h2>
                 </div>
                 <form className="mt-8 space-y-6" action="#" method="POST">
